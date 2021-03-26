95df3f549f89aed63cc1e4eb5947971d495d67ac
SuperPox <calebkohl@gmail.com>
2021-03-26 16:29:49 -0600

    changes model names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
     
-    has_many :followers, foreign_key: :follower_id , class_name: "Connection"
-    has_many :followed, through: :followers
-    has_many :followed, foreign_key: :followed_id, class_name: "Connection"
-    has_many :followers, through: :followed
+    has_many :match_you, foreign_key: :match_you_id , class_name: "Connection"
+    has_many :you_match, through: :match_you
+
+    has_many :you_match, foreign_key: :you_match_id, class_name: "Connection"
+    has_many :match_you, through: :you_match
 
     has_secure_password
     validates_uniqueness_of :username
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   resources :connections
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  
+  
+  get '/signup', to: "users#new", as: "signup"
+  post '/signup', to: "users#create"
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
+  get "/auth/facebook/callback", to: "sessions#create_with_fb"
+
 end
