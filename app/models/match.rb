2570d34b4493bb1b990c614d5b641c6f313fdcd4
SuperPox <calebkohl@gmail.com>
2021-03-26 15:59:34 -0600

    re-did the model

--- app/models/match.rb
+++
@@ -1,6 +0,0 @@
-class Match < ActiveRecord::Base
-
-    belongs_to :match_you, class_name: "User"
-    belongs_to :you_match, class_name: "User"
-
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
-class User < ActiveRecord::Base
-    has_secure_password
+class User < ApplicationRecord
+    
+    has_many :followers, foreign_key: :follower_id , class_name: "Connection"
+    has_many :followed, through: :followers
+    has_many :followed, foreign_key: :followed_id, class_name: "Connection"
+    has_many :followers, through: :followed
 
-    has_many :match_you, foreign_key: :match_you_id , class_name: "Match"
-    has_many :you_match, through: :match_you
-    has_many :you_match, foreign_key: :you_match_id, class_name: "Match"
-    has_many :match_you, through: :you_match
+    has_secure_password
+    validates_uniqueness_of :username
+    validates_presence_of :username
 
 end
-
