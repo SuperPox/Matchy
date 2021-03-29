e7fd52c24235175bd12ac73fe9718c95204c6032
SuperPox <calebkohl@gmail.com>
2021-03-29 11:44:58 -0600

    adds nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :connections
-  resources :users
+
+  resources :users, except: [:new, :create] do
+    resources(:connections)
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  
-  
   get '/signup', to: "users#new", as: "signup"
   post '/signup', to: "users#create"
   get '/login', to: "sessions#new", as: "login"
