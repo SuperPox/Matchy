49b0c674302f1bfa1e9bba1cafb9e4fbff7fb039
SuperPox <calebkohl@gmail.com>
2021-03-30 23:03:18 -0600

    day of failures and nothing

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,8 +1,36 @@
 class ConnectionsController < ApplicationController
 
+    
+    
+    
+    
+    
     def index
+        #@user = User.find_by(id: params[:id])
+        #@my_matches = @user.find_my_matches
         
-        @user = User.find_by(id: params[:id])
+        @user = User.last
+        @user.find_my_matches
+
+        @over_30 = User.older_than_30
+    end
+
+    def create
+        @connection = current_user.connections.build(:connection_id => params[:connection_id])
+        if @connection.save
+            flash[:notice] = "successfully created connection"
+            redirect_to root_url
+        else
+            flash[:error] = "unable to add connection"
+            render :action => 'new'
+        end
+    end
+
+    def destroy
+        @connection = current_user.connections.find(params[:id])
+        @connection.destroy
+        flash[:notice] = "successfully destroyed connection"
+        redirect_to root_url
     end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,65 @@
 class User < ApplicationRecord
     
+    # has_many :match_you, foreign_key: :match_you_id , class_name: "Connection"
+    # has_many :you_match, through: :match_you
+
+    # has_many :you_match, foreign_key: :you_match_id, class_name: "Connection"
+    # has_many :match_you, through: :you_match
+
     has_many :match_you, foreign_key: :match_you_id , class_name: "Connection"
-    has_many :you_match, through: :match_you
+    has_many :you_matched, through: :match_you
 
     has_many :you_match, foreign_key: :you_match_id, class_name: "Connection"
-    has_many :match_you, through: :you_match
+    has_many :matched_you, through: :you_match
 
     has_secure_password
     validates_uniqueness_of :username
     validates_presence_of :username
 
+    scope :younger_than_30, -> { where ("(age) <= 30")}
+    scope(:older_than_30, ->(age = 30) { self.where("age > ?", age) })
+
+    def find_my_matches
+        my_spirituality_matches = User.where(spirituality: "#{self.spirituality}").ids  
+
+        @connection = Connection.new
+        binding.pry
+
+        @connection.id = self.id
+
+
+
+
+
+
+
+
+
+
+        
+        
+        my_connection = Connection.create()
+
+        @connection = self.connection.build(:you_match_id)
+        
+        
+
+        
+        self.you_match << my_spirituality_matches
+
+
+
+
+        
+        binding.pry
+
+    end
+
+
+
+
+
 end
+
+
+
